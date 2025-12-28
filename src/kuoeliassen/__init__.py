bca9bfc7747d8c9036a91b900952f23d4835cf1a
Qianye Su <976113615@qq.com>
2025-12-28 21:05:27 +0800

    Update the project version to 0.2.2 and dynamically read the version information from pyproject.toml.

--- src/kuoeliassen/__init__.py
+++ src/kuoeliassen/__init__.py
@@ -6,8 +6,7 @@ from .core import solve_ke, solve_ke_LHS
 from .xarray_interface import solve_ke_xarray, solve_ke_LHS_xarray
 import re
 from pathlib import Path
-_pyproject = Path(__file__).parent.parent.parent / \
-    "pyproject.toml"
+_pyproject = Path(__file__).parent.parent.parent / "pyproject.toml"
 __version__ = re.search(  # Read version from pyproject.toml
     r'version\s*=\s*"([^"]+)"', _pyproject.read_text()).group(1)
 __author__ = "Qianye Su"
